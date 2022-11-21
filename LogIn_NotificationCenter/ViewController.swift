0e9123cf9ebdc07261bd6e2ecc2ed6bae20bfa9d
marcoweinmann <marco.wm94@gmail.com>
2022-11-21 11:17:51 +0100

    Initial Commit

---
+++ LogIn_NotificationCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogIn_NotificationCenter
+//
+//  Created by Marco Weinmann on 21.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
